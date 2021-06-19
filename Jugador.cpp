49e05fa3b750324960800c05c0489b28a04fd4ad
nil <fernandexnil@gmail.com>
2021-06-19 22:34:55 +0200

    Jugador.cpp error corregido

--- Jugador.cpp
+++ Jugador.cpp
@@ -6,73 +6,76 @@ void Jugador::afegeixCarta(const Carta& carta)
 	m_cartes.push_back(carta);
 }
 
-bool Jugador::tiraCarta(const Carta& cartaPila, Carta& cartaATirar)
+bool Jugador::comprovaCarta(const Carta& carta, Carta& cartaATirar)
 {
-	//SELECCIONAR CARTA DE M_CARTES
-	//CARTA PILA ES LA ULTIMA CARTA JUGADA
-	//SI LA CARTA ES POT TIRAR LA IGAUALAREM A CARTAATIRAR Y M_HAPOGUTTIRAR = TRUE
-	//SI NO M_HAPOGUTTIRAR = FALSE
-	//SI CARTES POSIBLES > 1 BUSCAR LA MEJOR
-
 	list<Carta>::iterator aux = m_cartes.begin();
-	list<Carta> auxColor;
-	Carta carta;
-	bool valorIgual = false;
-	bool color = false;
-	bool robaDos = false;
-	bool saltaTorn = false;
-	for(; aux != m_cartes.end();aux++){
-		if(cartaPila.getColor() == aux->getColor() && cartaPila.getValor() != SALTA_TORN){
-			color = true;
-			auxColor.push_back(*aux);
-			carta = *aux;
+	list<Carta>::iterator itCartaSeleccionada = m_cartes.end();
+	cartaATirar = carta;
+	bool trobat = false;
+	while (aux != m_cartes.end())
+	{
+		if (((aux->getColor() == carta.getColor()) || (carta.getColor() == -1)) &&
+			((aux->getValor() == carta.getValor()) || (carta.getValor() == -1)))
+		{
+			trobat = true;
 			m_haPogutTirar = true;
-		}else if(cartaPila.getValor() == aux->getValor() && cartaPila.getValor() != SALTA_TORN && !color){
-			m_haPogutTirar = true;
-			carta = *aux;
-			valorIgual = true;
-		}
-	}
-
-	if(auxColor.size() > 1){
-		list<Carta>::iterator it_auxColor = auxColor.begin();
-		for(;it_auxColor != auxColor.end();it_auxColor++){
-			if(it_auxColor->getValor() == ROBA_DOS){
-				robaDos = true;
-				cartaATirar = *it_auxColor;
-				//m_cartes.remove(*it_auxColor);
-			}else if(it_auxColor->getValor() == CANVI_TORN && !robaDos){
-				saltaTorn = true;
-				cartaATirar = *it_auxColor;
-				//m_cartes.remove(*it_auxColor);
-
-			}else if(it_auxColor->getValor() == SALTA_TORN && !robaDos && !saltaTorn){
-				saltaTorn = true;
-				cartaATirar = *it_auxColor;
-				//m_cartes.remove(*it_auxColor);
-
+			if (carta.getValor() == -1)
+			{
+				if ((itCartaSeleccionada == m_cartes.end()) || (aux->getValor() < cartaATirar.getValor()))
+				{
+					cartaATirar = *aux;
+					itCartaSeleccionada = aux;
+				}
 			}
+			else
+				if (carta.getColor() == -1)
+				{
+					if ((itCartaSeleccionada == m_cartes.end()) || (aux->getColor() < cartaATirar.getColor()))
+					{
+						cartaATirar = *aux;
+						itCartaSeleccionada = aux;
+					}
+				}
+				else
+				{
+					cartaATirar = *aux;
+					itCartaSeleccionada = aux;
+				}
 		}
-	}else if(auxColor.size() == 1){
-		//m_cartes.remove(carta);
-		cartaATirar = carta;
-	}else if(valorIgual){
-		//m_cartes.remove(carta);
-		cartaATirar = carta;
+		aux++;
 	}
-
-	return m_haPogutTirar;
+	if (trobat)
+		m_cartes.erase(itCartaSeleccionada);
+	return trobat;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+bool Jugador::tiraCarta(const Carta& cartaPila, Carta& cartaATirar)
+{
+	m_haPogutTirar = false;
+	Carta carta;
+	carta.setColor(cartaPila.getColor());
+	carta.setValor(ROBA_DOS);
+	bool potTirar = comprovaCarta(carta, cartaATirar);
+	if (!potTirar)
+	{
+		carta.setValor(CANVI_TORN);
+		potTirar = comprovaCarta(carta, cartaATirar);
+		if (!potTirar)
+		{
+			carta.setValor(SALTA_TORN);
+			potTirar = comprovaCarta(carta, cartaATirar);
+			if (!potTirar)
+			{
+				carta.setValor(-1);
+				potTirar = comprovaCarta(carta, cartaATirar);
+				if (!potTirar)
+				{
+					carta.setColor(-1);
+					carta.setValor(cartaPila.getValor());
+					potTirar = comprovaCarta(carta, cartaATirar);
+				}
+			}
+		}
+	}
+	return m_haPogutTirar;
+}
